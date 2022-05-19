3c70896637a7a378aca6b4dd713570291473f932
Armenak Shakaryan <armenak.shakaryan@impaladigital.com>
2022-05-19 17:45:56 +0400

    Preparation for deploying to PyPi test

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
-from setuptools import find_packages, setup
-
+from distutils.core import setup
 
 setup(
     name='socket_project',
-    packages=find_packages(include=['socket_project']),
+    packages=['my_python_package'],
     version='1.0',
     description='My test library',
     author='armen1337',
-    license='MIT',
+    author_email='armen14102003@mail.ru',
+    url='https://github.com/armen1337/socket-project',
+    download_url='download link you saved',
+    keywords=[],
+    classifiers=[],
 )
+
+# https://www.codementor.io/@arpitbhayani/host-your-python-package-using-github-on-pypi-du107t7ku
+# https://medium.com/analytics-vidhya/how-to-create-a-python-library-7d5aea80cc3f
