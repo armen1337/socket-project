b1a1d03072e32e9e3b970f16624399cd74a609f4
Armenak Shakaryan <armenak.shakaryan@impaladigital.com>
2022-05-21 14:46:30 +0400

    Preparation for deploying

--- build/lib/socket_project/test_connect.py
+++
@@ -1,8 +0,0 @@
-from server.client import Client
-
-
-client = Client("localhost", 8000)
-
-client.connect()
-# client.send_message(input("Type your message: "))
-client.send_message("q")
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='socket_project',
     packages=['socket_project'],
-    version='0.1-alpha',
+    version='0.2',
     description='My test library',
     author='armen1337',
     author_email='armen14102003@mail.ru',
@@ -15,4 +15,3 @@ setup(
 
 # https://www.codementor.io/@arpitbhayani/host-your-python-package-using-github-on-pypi-du107t7ku
 # https://medium.com/analytics-vidhya/how-to-create-a-python-library-7d5aea80cc3f
-# https://github.com/armen1337/socket-project/archive/refs/tags/v0.1-alpha.tar.gz
